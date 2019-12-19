ae20f4b7bccff531d1a3f3b0c0d5653fbfa1e7b1
Sunic <gitsunic@mail.ru>
2019-12-19 16:56:16 +0300

    settings

--- session/settings.py
+++ session/settings.py
@@ -109,10 +109,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
