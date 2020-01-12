459eae3f4381b776d25e8e4f47f3229a2ce0bf0f
Sunic <gitsunic@mail.ru>
2020-01-12 15:43:40 +0300

    physics tickets 1.1

--- physics/urls.py
+++ physics/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('min', views.min, name='minimum'),
-    path('pp_add', views.page_add, name='page_add'),
+    path('mp_add', views.page_add, name='page_add'),
     path('t_add', views.ticket_add, name='ticket_add'),
     path('<int:id>', views.ticket_view, name='ticket'),
     path('', views.p_start, name='p_start'),
